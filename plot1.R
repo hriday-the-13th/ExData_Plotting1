d51880e88df9be51829859f29fc8d23b72811c91
hriday-the-13th <hriday13patgiri@gmail.com>
2020-04-27 18:12:36 +0530

    code for plot 1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+###R code for plot1
+###reading the data
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+###creating a .png file
+png("plot1.png", width=480, height=480)
+
+###ploting the histogram
+hist(globalActivePower, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.off()
